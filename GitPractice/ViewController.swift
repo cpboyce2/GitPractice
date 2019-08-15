a0b842b9dfc06d92083ccfbeec0763fb374e1038
connor_boyce <cpboycercf@gmail.com>
2019-08-15 13:43:11 -0400

    Initial Commit

---
+++ GitPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitPractice
+//
+//  Created by Connor Boyce on 8/15/19.
+//  Copyright © 2019 Connor Boyce. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
